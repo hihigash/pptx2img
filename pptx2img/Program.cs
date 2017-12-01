60127a5bfe0ca9de569a0cd64bf253b65362dcba
hihigash <hihigash@microsoft.com>
2017-12-01 15:36:27 +0900

    プロジェクト ファイルを追加します。

---
+++ pptx2img/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using CommandLine;
+using CommandLine.Text;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace pptx2img
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('f', "FileName", Required = true, HelpText = "PowerPoint (*.pptx) file name")]
+            public string FileName { get; set; }
+
+            [Option('o', "OutDir", Required = true, HelpText = "Output directory")]
+            public string OutDir { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                return HelpText.AutoBuild(this, _ => HelpText.DefaultParsingErrorsHandler(this, _));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Options options = new Options();
+            if (!Parser.Default.ParseArguments(args, options))
+            {
+                return;
+            }
+
+            Application application = null;
+            try
+            {
+                application = new Application();
+                var fileName = Path.GetFullPath(options.FileName);
+                var presentation = application.Presentations.Open(fileName,
+                    MsoTriState.msoFalse, MsoTriState.msoFalse, MsoTriState.msoFalse);
+
+                if (!Directory.Exists(options.OutDir))
+                {
+                    Directory.CreateDirectory(options.OutDir);
+                }
+
+                var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(options.FileName);
+                int i = 1;
+                foreach (Slide slide in presentation.Slides)
+                {
+                    var shape = slide.Shapes.Range(slide.Shapes.GetIndices()).Group();
+                    shape.Export(Path.Combine(Path.GetFullPath(options.OutDir), $"{fileNameWithoutExtension}_{i}.png"), PpShapeFormat.ppShapeFormatPNG);
+                    i++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                application?.Quit();
+            }
+        }
+    }
+}
---
+++ pptx2img/ShapesExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Office.Interop.PowerPoint;
+
+namespace pptx2img
+{
+    public static class ShapesExtension
+    {
+        public static int[] GetIndices(this Shapes shapes)
+        {
+            int[] result = new int[shapes.Count];
+            for (int i = 1; i <= shapes.Count; i++)
+            {
+                result[i - 1] = i;
+            }
+
+            return result;
+        }
+    }
+}
